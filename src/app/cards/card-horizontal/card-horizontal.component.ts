cd332e6f957566ecaee37feafdd5d56a01d796fa
Jasmine Quach <xjasminequach@gmail.com>
2018-02-14 21:39:45 -0600

    Passing data into components WIP

--- src/app/cards/card-horizontal/card-horizontal.component.ts
+++ src/app/cards/card-horizontal/card-horizontal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-horizontal',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-horizontal.component.scss']
 })
 export class CardHorizontalComponent implements OnInit {
+
+  @Input()  position: string;
 
   constructor() { }
 
